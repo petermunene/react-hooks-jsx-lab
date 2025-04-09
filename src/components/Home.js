d6bcbd70c508dabff9634bc1c75185d125d0394c
petermunene <peter.munene@student.moringaschool.com>
2025-04-09 14:50:50 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div>Home
+    <h1 style={{color:"firebrick"}}  id="home">{name} is a Web Developer from {city}</h1>
+
+  </div>
+         
+
 }
 
 export default Home;
